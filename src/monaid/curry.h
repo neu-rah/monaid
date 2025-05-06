b2c58be0a3390a4c22311c66bb107ebaa8802368
neu-rah <ruihfazevedo@gmail.com>
2025-05-06 08:07:35 +0000

    no fw of const&

--- src/monaid/curry.h
+++ src/monaid/curry.h
@@ -4,11 +4,11 @@
 #include "monaid/base.h"
 
 // curry --
-constexpr const auto $(const auto&& f,auto&&... oo)
+constexpr const auto $(const auto& f,auto&&... oo)
   requires std::is_invocable_v<decltype(f),decltype(oo)...> 
-  {return std::invoke(fw$(f),fw$$(oo));}
+  {return std::invoke(f,fw$$(oo));}
 
-constexpr const auto $(const auto&& f,auto&&... oo)
+constexpr const auto $(const auto& f,auto&&... oo)
   requires(!std::is_invocable_v<decltype(f),decltype(oo)...>)
-  {return [&f,&oo...](auto&&... pp) {return $(fw$(f),fw$$(oo),fw$$(pp));};}
+  {return [&f,&oo...](auto&&... pp) {return $(f,fw$$(oo),fw$$(pp));};}
 
