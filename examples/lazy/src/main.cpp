43a6c547369771e12124c504532806e8ef07226c
neu-rah <ruihfazevedo@gmail.com>
2025-07-25 02:25:31 +0000

    nicest beta til now

--- examples/lazy/src/main.cpp
+++ examples/lazy/src/main.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 #include <monaid/curry.h>
+#include "monaid/maybe.h"
 
 template<typename O,typename... OO> struct Prog;
 
 template<typename O> struct Prog<O> {
   using Op=O;
   const Op op;
+  template<typename F, typename... OO>
+  const auto beta(const F f,const OO...oo) const {
+    if constexpr (std::is_invocable_v<
+        decltype(F::template beta<OO...,Op>),
+        const decltype(oo)...,
+        const decltype(op)
+    >) return Just(f.beta(oo...,op));
+    else return mempty;
+  }
 };
 
 template<typename O,typename... OO> 
@@ -17,19 +27,41 @@ struct Prog:Prog<O> {
   const Tail tail;
   constexpr Prog(const O o,const OO... oo):Prog<O>{o},tail{oo...}{}
   constexpr Prog(const O o,const Tail oo):Prog<O>{o},tail{oo}{}
+  const auto beta() const {return tail.beta(op);}
+  const auto beta(const auto... oo) const {return tail.beta(oo...,op);}
 };
 
-template<typename C> struct Combinator {};
+template<typename C> struct Combinator {
+  static constexpr const auto beta(const auto...) {return mempty;}
+};
 
 struct I:Combinator<I> {
+  // using Combinator<I>::beta;
   static constexpr const auto beta(const auto o) {return o;}
 };constexpr const I id;
 
-Prog<int,bool,const char*> x{1,true,"ok"};
+struct K:Combinator<K> {
+  // using Combinator<K>::beta;
+  static constexpr const auto beta(const auto o,const auto) {return o;}
+};constexpr const K _true;
 
+template<typename Out,typename O>
+Out& operator<<(Out& out,const Prog<O> o) {return out<<o.op;}
+template<typename Out,typename O,typename... OO>
+Out& operator<<(Out& out,const Prog<O,OO...> o) {return out<<o.op<<" "<<o.tail;}
 template<typename Out> Out& operator<<(Out& out,const I) {return out<<"id";}
+template<typename Out> Out& operator<<(Out& out,const K) {return out<<"true";}
+
+namespace test {
+  constexpr const Prog id(::id,"ok");
+  constexpr const Prog _true(::_true,"ok","false");
+  constexpr const Prog fail(::_true,"ok");
+};
 
 int main() {
-  cout<<id<<endl;
- return 0;
+  cout<<test::id<<" => ";
+  cout<<test::id.beta()<<endl;
+  cout<<test::_true<<" => "<<test::_true.beta()<<endl;
+  // cout<<test::fail<<" => "<<test::fail.beta()<<endl;
+  return 0;
 }
--- src/monaid/params.h
+++ src/monaid/params.h
@@ -1,13 +1,20 @@
 #pragma once
 
+/// @brief holds an abstract empty "value" still waiting for its final type
+///   this is the support of mempty
 struct Empty {
   using Type=Empty;
+  /// @brief converts to anything
+  /// @tparam K the anything type
   template<typename K> constexpr operator K() const {return K();}
   auto fmap(const auto&) const {return *this;}
   auto bind(const auto&) const {return *this;}
 };
 template<typename Out> Out& operator<<(Out& out,const Empty&) {return out<<"ø";}
 
+/// @brief holds a monadic value still waiting for the final type, it can then convert to Either or Maybe, ...
+///   this is the (data transport) support for `pure` implementation
+/// @tparam O value type
 template<typename O>
 struct Param {
   using Type=O;
@@ -16,6 +23,8 @@ struct Param {
   auto fmap(const auto& f) const {return Param<decltype(f(m_param))>(f(m_param));}
   auto bind(const auto& f) const {return f(m_param);}
 };
+
+/// @brief print the transient data
 template<typename Out,typename O>
 Out& operator<<(Out& out,const Param<O>& o)
   {return out<<"{"<<o.m_param<<"}";}
